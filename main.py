5b792df341cec383440980421fb97bdb4f81077e
wayanadi26 <88713651+wayanadi26@users.noreply.github.com>
2023-10-15 16:13:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ sns.set(style='dark')
 # Dataset
 df = pd.read_csv('all_data.csv')
 
-st.title('Konsentrasi Distribusi PM2.5 diudara')
+st.title('Konsentrasi Distribusi PM2.5 di udara')
 
 plt.figure(figsize=(12, 5))
 sns.histplot(data=df, x='PM2.5', bins=50, kde=True)
-# plt.title('Distribution of the recorded PM2.5 concentration in the air', fontsize=16)
 
 st.pyplot(plt)
 
@@ -26,7 +25,19 @@ for i, j in enumerate(months):
     map_dict.setdefault(i+1, j)
 
 monthly_data['month'] = monthly_data['month'].map(map_dict)
-monthly_average = monthly_data.groupby('month').median()
+
+# Check for non-numeric values in the 'PM2.5' column
+non_numeric_values = monthly_data[~monthly_data['PM2.5'].apply(lambda x: str(x).replace('.', '', 1).isdigit())]
+
+# If there are non-numeric values, you may want to inspect and clean the data
+if not non_numeric_values.empty:
+    st.write("Non-numeric values found in the 'PM2.5' column:")
+    st.write(non_numeric_values)
+
+# Convert the 'PM2.5' column to numeric
+monthly_data['PM2.5'] = pd.to_numeric(monthly_data['PM2.5'], errors='coerce')
+
+monthly_average = monthly_data.groupby('month')['PM2.5'].median()
 monthly_average = pd.merge(
     ordered_monthdf, monthly_average, left_on='month', right_index=True)
 monthly_average = np.round(monthly_average, 1)
@@ -38,7 +49,6 @@ plt.figure(figsize=(12, 6))
 sns.barplot(data=monthly_data, x='month', y='PM2.5', hue='station', ci=None)
 plt.xlabel('Month', fontsize=14)
 plt.ylabel('Konsentrasi PM2.5 (ug/m^3)', fontsize=14)
-# plt.title('Jumlah PM2.5 yang terkandung di udara', fontsize=16)
 plt.grid(axis='x')
 plt.tight_layout()
 
@@ -52,13 +62,11 @@ wind_dir = df[['wd', 'PM2.5']]
 
 wind_dir = wind_dir.groupby('wd').median()
 
-
 with plt.style.context('ggplot'):
     plt.figure(figsize=(12, 5))
     wind_dir.plot(legend=False, kind='bar', linewidth=0.9)
     plt.xlabel('Arah udara', fontsize=14)
     plt.ylabel('Konsentrasi PM2.5 (ug/m^3)', fontsize=14)
-    # plt.title('Arah pergerakan udara yang tercemar', fontsize=16)
     plt.grid(axis='x')
     plt.tight_layout()
 
@@ -71,8 +79,6 @@ correlation_data = df[['PM2.5', 'PM10', 'SO2', 'NO2',
                        'CO', 'O3', 'TEMP', 'PRES',
                        'DEWP', 'RAIN', 'WSPM']]
 sns.heatmap(correlation_data.corr(), cmap=plt.cm.Reds, annot=True)
-# plt.title('Korelasi Matrix antar variable', fontsize=16)
-plt.show()
 st.pyplot(plt)
 
-st.write("Terdapat 3 variable yang memiliki keterkaitan untuk mempengaruhi Kualitas PM2.5, yaitu PM10, NO2 dan CO, karena memiliki korelasi posiitif untuk mempengaruhi peningkatan PM2.5.")
+st.write("Terdapat 3 variable yang memiliki keterkaitan untuk mempengaruhi Kualitas PM2.5, yaitu PM10, NO2, dan CO, karena memiliki korelasi positif untuk mempengaruhi peningkatan PM2.5.")
